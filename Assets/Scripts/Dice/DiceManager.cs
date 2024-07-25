7c1d10d76c29a00f4af4b44902a2e2afbc5dee6b
Mehmet Yalçin <mehmetyalcin0040@gmail.com>
2024-07-25 18:50:17 +0300

    solider fbx added

--- Assets/Scripts/Dice/DiceManager.cs
+++ Assets/Scripts/Dice/DiceManager.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class DiceManager : MonoBehaviour
 {
     public static DiceManager Instance;
+
+
     public List<GameObject> activeRivalDiceLists = new List<GameObject>();
     public List<GameObject> activePlayerDiceLists = new List<GameObject>();
+
+    public List<GameObject> activeRivalDiceSortedLists = new List<GameObject>();
+    public List<GameObject> activePlayerDiceSortedLists = new List<GameObject>();
+
     public List<int> activeRivalDiceValueSortedLists= new List<int>();
     public List<int> activePlayerDiceValueSortedLists = new List<int>();
 
@@ -53,9 +59,9 @@ public class DiceManager : MonoBehaviour
     
     public IEnumerator DiceDisactiveted()
     {
-        Debug.LogWarning(" dis aktivated 5 sn sonra baþlayacak");
+       // Debug.LogWarning(" dis aktivated 5 sn sonra baþlayacak");
         yield return new WaitForSeconds(attackDuration);
-        Debug.LogWarning(" dis aktivated baþladý");
+       // Debug.LogWarning(" dis aktivated baþladý");
         activePlayerDiceLists.Clear();
         activeRivalDiceLists.Clear();
         for (int i = 0; i < 4; i++)
@@ -78,16 +84,43 @@ public class DiceManager : MonoBehaviour
     }
     public void  SortDiceList()
     {
+        // player ve rival zarlarý listelere eklendi 
         foreach (var dice in activeRivalDiceLists)
         {
             activeRivalDiceValueSortedLists.Add(dice.gameObject.GetComponent<DiceRollerDO>().DiceValue);
         }
+
         foreach (var dice in activePlayerDiceLists)
         {
             activePlayerDiceValueSortedLists.Add(dice.gameObject.GetComponent<DiceRollerDO>().DiceValue);
         }
+        // player ve rivall zarlarý sýralandý
+
         activeRivalDiceValueSortedLists.Sort();
-        activeRivalDiceValueSortedLists.Sort();
+        activePlayerDiceValueSortedLists.Sort();
+
+        // sýralý listedeki sýraya göre zarlar yeniden listelendi
+        for( int i=0; i< activeRivalDiceLists.Count; i++)
+        {
+            for( int j=0;j< activeRivalDiceLists.Count; j++)
+            {
+                if (activeRivalDiceLists[j].GetComponent<DiceRollerDO>().DiceValue == activeRivalDiceValueSortedLists[i])
+                {
+                    activeRivalDiceSortedLists.Add(activeRivalDiceLists[j]);
+                }
+            }
+        }
+
+        for (int i = 0; i < activePlayerDiceLists.Count; i++)
+        {
+            for (int j = 0; j < activePlayerDiceLists.Count; j++)
+            {
+                if (activePlayerDiceLists[j].GetComponent<DiceRollerDO>().DiceValue == activePlayerDiceValueSortedLists[i])
+                {
+                    activePlayerDiceSortedLists.Add(activePlayerDiceLists[j]);
+                }
+            }
+        }
     }
     
 }
