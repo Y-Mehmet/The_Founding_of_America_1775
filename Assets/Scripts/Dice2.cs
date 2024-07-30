e11aaa0a856aa99b0dc9df387bceb798572b1ff0
Mehmet Yalçin <mehmetyalcin0040@gmail.com>
2024-07-30 17:35:56 +0300

    dice2 update

--- Assets/Scripts/Dice2.cs
+++ Assets/Scripts/Dice2.cs
@@ -7,29 +7,32 @@ public class Dice2 : MonoBehaviour
 {
     // Hedef konumu belirleyin
     public Transform target;
-    public float zOffset = 30;
+    public float zOffset = -30;
     public bool isRaceRoll = true;
     // Hareket süresi
-    public float duration = 3f;
+     float duration = 1f;
     Vector3 startPoint;
     // Sallama süresi
     public float shakeDuration = 1f;
     // Sallama gücü
-    public float shakeStrength = 2f;
+    public float shakeStrength = 1f;
     // Sallama titreþim sayýsý
-    public int vibrato = 20;
+    public int vibrato = 10;
     // Rastgelelik miktarý
-    public float randomness = 140f;
+    public float randomness = 90;
+    Vector3 firstScale;
     void Start()
     {
+        firstScale= transform.localScale;
         InvokeRepeating("CallRaceRoll",0 , duration + 3);
 
         startPoint = transform.position;
+
     }
 
     void CallRaceRoll()
     {
-        transform.localScale = Vector3.one;
+        transform.localScale = firstScale;
         // Kontrol noktalarýný belirleyin
         Vector3 midPoint = new Vector3((startPoint.x + target.position.x) / 2, (startPoint.y + target.position.y) / 2, (startPoint.z + zOffset)); // Yükselme noktasý
         Vector3 endPoint = target.position;
@@ -42,8 +45,8 @@ public class Dice2 : MonoBehaviour
             .SetEase(Ease.Linear)
             .OnComplete(() => {
                 // Hareket tamamlandýðýnda boyutu iki katýna çýkarma
-                transform.DOShakePosition(duration+1, shakeStrength, vibrato, randomness);
-                transform.DOScale(10, duration+1); // Burada 1f, ölçeklenmenin süresini belirtir
+                transform.DOShakeRotation(duration+1, shakeStrength, vibrato, randomness);
+                transform.DOScale(firstScale.x*10, duration+1); // Burada 1f, ölçeklenmenin süresini belirtir
             });
     }
 }
